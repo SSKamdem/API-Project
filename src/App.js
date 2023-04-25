2a043a0850b657855197d421fef7dbdb07c3c2e1
Sarah Kamdem <sarahsimok@gmail.com>
2023-04-25 12:26:14 -0400

    changed the api and planned my website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const options = {
+    method: 'GET',
+    url: 'https://tasty.p.rapidapi.com/recipes/list',
+    params: {
+      from: '0',
+      size: '50'
+    },
+    headers: {
+      'content-type': 'application/octet-stream',
+      'X-RapidAPI-Key': 'ca62767eafmsha5866dcb5113f67p17d1c7jsncb2a078e4dc4',
+      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
